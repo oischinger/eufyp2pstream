ddc46685ad0dd3d63a6da1c3757090ad7d09286e
Hans Oischinger <hans.oischinger@gmail.com>
2022-09-01 20:08:09 +0200

    Backchannel & Single-Frame webserver

--- README.md
+++ README.md
@@ -6,14 +6,24 @@ It uses [go2RTC](https://github.com/AlexxIT/go2rtc) to provide the live stream.
 
 # Howto install
 
+## Prerequisites
+Install and configure the [Eufy Security Addon](https://github.com/fuatakgun/eufy_security_addon) first. See the instructions [here](https://github.com/fuatakgun/eufy_security).
+
+## Install this addon
 Copy this directory to /addons/eufyp2pstream and run it.
 
-Open the WebUI: http://HOME_ASSISTANT_HOST::1984/webrtc.html?src=camera1 and enjoy a camera stream with audio via WEBRTC.
+Open the [Addon's WebUI](https://my.home-assistant.io/redirect/supervisor_ingress/?addon=eufyp2pstream) and enjoy a camera stream with audio via WEBRTC.
 
 # References
 This project is inspired by:
 
-[eufy_security Home Assistant Custom Component](https://github.com/fuatakgun/eufy_security)
-[go2RTC](https://github.com/AlexxIT/go2rtc)
-[Video Decoding in ioBroker.eusec](https://github.com/bropat/ioBroker.eusec/blob/0a15e1d125f4fd00144af66d57d8d738140ea619/src/lib/eufy-security/video.ts#L14-L65
+- [eufy_security Home Assistant Custom Component](https://github.com/fuatakgun/eufy_security)
+- [go2RTC](https://github.com/AlexxIT/go2rtc)
+- [Video Decoding in ioBroker.eusec](https://github.com/bropat/ioBroker.eusec/blob/0a15e1d125f4fd00144af66d57d8d738140ea619/src/lib/eufy-security/video.ts#L14-L65
 )
+
+# TODO
+
+## Screenshot:
+
+`ffmpeg -f h264 -i tcp://127.0.0.1:63336?timeout=100000000 -strict -2  -hls_init_time 0 -hls_time 2 -hls_segment_type mpegts -fflags genpts+nobuffer+flush_packets -frames:v 1  test.jpg`
--- files/hello.py
+++ files/hello.py
@@ -7,10 +7,18 @@ import socket
 import threading
 import time
 import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 from queue import Queue
+
+HOST_NAME = '0.0.0.0'
+PORT_NUMBER = 8787
+
+RTSP_PORT_NUMBER = 8541
 
 video_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 audio_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+backchannel_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 EVENT_CONFIGURATION: dict = {
@@ -75,11 +83,14 @@ class ClientAcceptThread(threading.Thread):
                 self.queues.remove(thread.queue)
         self.my_threads = [t for t in self.my_threads if t.is_alive()]
         if self.ws and my_threads_before > 0 and len(self.my_threads) == 0:
-            print("All clients died. Stopping Stream for ", self.name)
+            if self.name == "BackChannel":
+                print("All clients died. Stopping Stream for ", self.name)
+            else:
+                print("All clients died. Stopping Stream for ", self.name)
 
-            msg = STOP_P2P_LIVESTREAM_MESSAGE.copy()
-            msg["serialNumber"] = self.serialno
-            asyncio.run(self.ws.send_message(json.dumps(msg)))
+                msg = STOP_P2P_LIVESTREAM_MESSAGE.copy()
+                msg["serialNumber"] = self.serialno
+                asyncio.run(self.ws.send_message(json.dumps(msg)))
 
     def run(self):
         print("Accepting connection for ", self.name)
@@ -91,14 +102,20 @@ class ClientAcceptThread(threading.Thread):
                 client_sock.setblocking(False)
                 print ("New connection added: ", client_addr, " for ", self.name)
                 sys.stdout.flush()
-                thread = ClientSendThread(client_sock, run_event, self.name, self.ws, self.serialno)
-                self.queues.append(thread.queue)
-                if self.ws:
-                    msg = START_P2P_LIVESTREAM_MESSAGE.copy()
-                    msg["serialNumber"] = self.serialno
-                    asyncio.run(self.ws.send_message(json.dumps(msg)))
-                self.my_threads.append(thread)
-                thread.start()
+
+                if self.name == "BackChannel":
+                    print("Starting BackChannel")
+                    thread = ClientRecvThread(client_sock, run_event, self.name, self.ws, self.serialno)
+                    thread.start()
+                else:
+                    thread = ClientSendThread(client_sock, run_event, self.name, self.ws, self.serialno)
+                    self.queues.append(thread.queue)
+                    if self.ws:
+                        msg = START_P2P_LIVESTREAM_MESSAGE.copy()
+                        msg["serialNumber"] = self.serialno
+                        asyncio.run(self.ws.send_message(json.dumps(msg)))
+                    self.my_threads.append(thread)
+                    thread.start()
             except socket.timeout:
                 pass
 
@@ -129,6 +146,30 @@ class ClientSendThread(threading.Thread):
             print("Timeout on socket for ", self.name)
             pass
 
+class ClientRecvThread(threading.Thread):
+    def __init__(self,client_sock,run_event,name,ws,serialno):
+        threading.Thread.__init__(self)
+        self.client_sock = client_sock
+        self.run_event = run_event
+        self.name = name
+        self.ws = ws
+        self.serialno = serialno
+
+    def run(self):
+        try:
+            while self.run_event.is_set():
+                try:
+                    data = self.client_sock.recv(1024)
+                    print("got ", len(data))
+                except BlockingIOError:
+                    # Resource temporarily unavailable (errno EWOULDBLOCK)
+                    pass
+        except socket.error as e:
+            print("Connection lost", self.name, e)
+            self.client_sock.close()
+        except socket.timeout:
+            print("Timeout on socket for ", self.name)
+            pass
 
 class Connector:
     def __init__(
@@ -143,6 +184,10 @@ class Connector:
         audio_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         audio_sock.settimeout(1) # timeout for listening
         audio_sock.listen()
+        backchannel_sock.bind(("127.0.0.1", 63338))
+        backchannel_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        backchannel_sock.settimeout(1) # timeout for listening
+        backchannel_sock.listen()
         self.ws = None
         self.run_event = run_event
         self.serialno = ""
@@ -173,8 +218,10 @@ class Connector:
                     self.serialno = state["serialNumber"]
                 self.video_thread = ClientAcceptThread(video_sock, run_event, "Video", self.ws, self.serialno)
                 self.audio_thread = ClientAcceptThread(audio_sock, run_event, "Audio", self.ws, self.serialno)
+                self.backchannel_thread = ClientAcceptThread(backchannel_sock, run_event, "BackChannel", self.ws, self.serialno)
                 self.audio_thread.start()
                 self.video_thread.start()
+                self.backchannel_thread.start()
 
         if message_type == "event":
             message = payload[message_type]
@@ -205,6 +252,21 @@ class Connector:
                     asyncio.run(self.ws.send_message(json.dumps(msg)))
 
 
+class MyHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "image/jpeg")
+        self.end_headers()
+
+    def do_GET(self):
+        """Respond to a GET request."""
+        self.send_response(200)
+        self.send_header("Content-type", "image/jpeg")
+        self.end_headers()
+        os.system('ffmpeg -y -i rtsp://127.0.0.1:' + str(RTSP_PORT_NUMBER) + '/camera1 -frames:v 1  /tmp/frame.jpg')
+        with open("/tmp/frame.jpg", "rb") as file:
+                self.wfile.write(file.read())
+
 async def main(run_event):
     c = Connector(run_event)
 
@@ -226,9 +288,17 @@ async def main(run_event):
     await asyncio.sleep(1000000000000000000000005)
 
 
+def http_serve():
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    httpd.serve_forever()
+
 run_event = threading.Event()
 run_event.set()
 try:
+    x = threading.Thread(target=http_serve)
+    x.start()
     asyncio.run(main(run_event))
 except (KeyboardInterrupt, SystemExit):
+    #httpd.server_close()
     run_event.clear()
